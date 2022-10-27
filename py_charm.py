9223f3fee13cfd152b8e47bfbf5865c64055dffd
Anton Petrovskyi <petrovskyyanton@gmail.com>
2022-10-27 20:41:58 +0300

    Commit 8. master

--- py_charm.py
+++ py_charm.py
@@ -2,3 +2,5 @@ def mane_function(x):
     print(x)
     for i in range(10):
         print(i**2)
+    dictionary = {'key_1': 1, 'key_2': 2, 'key_3': 3}
+    list_of_smth = [dictionary, 1234, None, f'String with {x}']
