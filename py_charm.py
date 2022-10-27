033caa543a34bfc15676fe96f0fbd412335232c3
Anton Petrovskyi <petrovskyyanton@gmail.com>
2022-10-27 20:42:25 +0300

    Commit 9. master

--- py_charm.py
+++ py_charm.py
@@ -4,3 +4,5 @@ def mane_function(x):
         print(i**2)
     dictionary = {'key_1': 1, 'key_2': 2, 'key_3': 3}
     list_of_smth = [dictionary, 1234, None, f'String with {x}']
+    set_of_smth = (1, 3, 24, f'{x}')
+    return list_of_smth, set_of_smth
